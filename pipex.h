8ef18e927e9133843a19b988d478cd28318f5a90
febranda <felipebrandaochaves@gmail.com>
2025-11-10 19:09:36 -0300

    .

--- pipex.h
+++ pipex.h
@@ -6,13 +6,18 @@
 /*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:22:33 by febranda          #+#    #+#             */
-/*   Updated: 2025/11/03 17:24:52 by febranda         ###   ########.fr       */
+/*   Updated: 2025/11/10 19:01:22 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include	<unistd.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int	verify_infile(const char *infile);
+int	verify_outfile(const char *outfile);
 
 #endif
