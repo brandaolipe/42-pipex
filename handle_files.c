a0fe723b1fec5467e7bff71fc8844e5924f5edc4
febranda <felipebrandaochaves@gmail.com>
2025-11-12 19:10:55 -0300

    rename functions and file

--- handle_files.c
+++ handle_files.c
@@ -6,13 +6,13 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 16:07:51 by febranda          #+#    #+#             */
-/*   Updated: 2025/11/10 19:21:37 by febranda         ###   ########.fr       */
+/*   Updated: 2025/11/12 18:44:52 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	verify_infile(const char *infile)
+int	get_infile_fd(const char *infile)
 {
 	int	fd;
 
@@ -25,7 +25,7 @@ int	verify_infile(const char *infile)
 	return (fd);
 }
 
-int	verify_outfile(const char *outfile)
+int	get_outfile_fd(const char *outfile)
 {
 	int	fd;
 
