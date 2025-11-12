010fbffa6687e9483bd90fb8bae92fd66cceca8e
febranda <felipebrandaochaves@gmail.com>
2025-11-12 19:12:31 -0300

    .

--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febranda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:22:33 by febranda          #+#    #+#             */
-/*   Updated: 2025/11/10 19:01:22 by febranda         ###   ########.fr       */
+/*   Updated: 2025/11/12 18:45:26 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-int	verify_infile(const char *infile);
-int	verify_outfile(const char *outfile);
+int	get_infile_fd(const char *infile);
+int	get_outfile_fd(const char *outfile);
 
 #endif
