f9114ae24be179e6bb07f3cc65aad1578bd36633
febranda <felipebrandaochaves@gmail.com>
2025-11-25 19:07:37 -0300

    .

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:22:33 by febranda          #+#    #+#             */
-/*   Updated: 2025/11/23 18:36:59 by febranda         ###   ########.fr       */
+/*   Updated: 2025/11/25 19:05:48 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 # include <sys/wait.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <sys/stat.h>
 # include <stdlib.h>
 # include "../libft/libft.h"
+# include <stdio.h>
 
 //int		get_infile_fd(const char *infile);
 //int		get_outfile_fd(const char *outfile);
@@ -27,6 +29,7 @@ char	**get_paths(char **envp);
 void	child_process_1(char *cmd1, char **envp, int *pipe_fd, char *infile);
 void	child_process_2(char *cmd2, char **envp, int *pipe_fd, char *outfile);
 void	error_message(void);
-char	*handle_path_and_cmd(char *cmd, char **paths)
+char	*handle_path_and_cmd(char *cmd, char **paths);
+void	execute_cmd(char *exec_path, char *cmd, char **envp);
 
 #endif
