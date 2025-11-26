aba245977e51bab8cb637f71cf0b443347dbf85c
febranda <felipebrandaochaves@gmail.com>
2025-11-26 20:24:06 -0300

    Add a exit() after execve()

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: febranda <febranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 16:28:42 by febranda          #+#    #+#             */
-/*   Updated: 2025/11/25 18:34:51 by febranda         ###   ########.fr       */
+/*   Updated: 2025/11/26 20:20:09 by febranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ char	*handle_path_and_cmd(char *cmd, char **paths)
 	while (paths[i])
 	{
 		partial_path = ft_strjoin(paths[i], "/");
-		// tenho que tratar cmd_argv para quando o nome do comando vir colado com
-		// as flags
 		exec_path = ft_strjoin(partial_path, cmd_argv[0]);
 		free(partial_path);
 		if (access(exec_path, X_OK) == 0)
@@ -49,4 +47,5 @@ void	execute_cmd(char *exec_path, char *cmd, char **envp)
 	exec_argv = ft_split(cmd, ' ');
 	execve(exec_path, exec_argv, envp);
 	free(exec_argv);
+	exit(1);
 }
